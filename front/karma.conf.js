62761f4daeb7df53ae75c7c2224d292b473b63e9
Arthur <arthur.levesque35@gmail.com>
2024-05-17 15:11:09 +0200

    Modification du github action

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -37,8 +37,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    //browsers: ['Chrome'],
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
